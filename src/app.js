5f2fbd3f0be6dd2f16edcd26258d74c16b47622f
amskyrobin <amskyrobinson@gmail.com>
2018-02-13 23:41:28 +0000

    initial setup renders hello world

---
+++ src/app.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+window.onload = function() {
+  ReactDOM.render(
+    <h1>hello world</h1>,
+    document.getElementById('app')
+    )
+}
