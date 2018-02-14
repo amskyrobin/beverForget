e70ab27d5807f4a2f2c6f1abcb1c6da15225ad07
amskyrobin <amskyrobinson@gmail.com>
2018-02-14 14:00:34 +0000

    mock data

---
+++ data/mockJSON.js
@@ -0,0 +1,43 @@
+var data = {
+  "drinkingEstablishment": [
+{
+  "Name": "Footlights",
+  "type": "bar"
+  "id": 1,
+  "drinks" {
+    "id": 1,
+    "name": "dark and stormy",
+    "price": 6
+  },
+  {
+      "id": 2,
+      "name": "gin and tonic",
+      "price": 8
+    },
+    {
+        "id": 3,
+        "name": "moscow mule",
+        "price": 7.20
+      }
+} ],
+[ {
+  "Name": "Chanter",
+  "type": "bar"
+  "id": 2,
+  "drinks" {
+    "id":4,
+    "name": "margarita"
+    "price": "5"
+  },
+  {
+      "id": 5,
+      "name": "tequila sunrise"
+      "price": 5.20
+    },
+   {
+        "id": 6,
+        "name": "brewdog"
+        "price": 5.90
+   }
+}]
+}
